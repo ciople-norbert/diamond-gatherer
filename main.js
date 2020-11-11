0ee82af1136089022589c8e0a59c857523d67fd4
ciople.norbert@gmail.com <ciople.norbert@gmail.com>
2020-11-11 19:38:00 +0200

    Curs 1 Introducere HTML, CSS, JS

---
+++ main.js
@@ -0,0 +1,16 @@
+var name = "Petey";
+var amount = 32;
+var knowsJs = true;
+var fruits = ["apple", "lemon", "orange"];
+
+function add(){
+    return 2 + 3;
+}
+
+var person = {
+    height: 2,
+    canWalk: function(){
+        console.log("Yes, I can walk");
+    },
+    name: "Kate"
+}
